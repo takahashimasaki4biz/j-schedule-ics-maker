eedb1f92b428514f5606502ac8d83212cfeedc0f
takahashimasaki4biz <takahashimasaki4biz@gmail.com>
2026-07-21 19:48:15 +0900

    ログにsoupを出力する条件を追加

--- j-schedule-ics-maker.py
+++ j-schedule-ics-maker.py
@@ -100,6 +100,8 @@ def get_soup(url):
 
 def get_match_tags1(soup):
     hiddenS4 = soup.find("div", id="S:4")
+    if not hiddenS4:
+        logging.info(soup)
     scheduleArea_tag = hiddenS4.find(
         "div", class_="p-game-schedule__list")
     return scheduleArea_tag.find_all("div", class_="p-game-schedule__list-item c-container", recursive=False)
